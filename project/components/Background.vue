686e0f751b9bef9d0b4d75c7316546f49849f50a
Y.Sasaki <y-sasaki@intworks.co.jp>
2019-03-30 01:40:07 +0900

    [add]TOPページの枠を作成

---
+++ project/components/Background.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='container'>
+
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 100%;
+  height: 100%;
+  background: red;
+}
+</style>
---
+++ project/components/Foreground.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='container'>
+
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 90%;
+  height: 90%;
+  background: black;
+  opacity: 0.3;
+  box-shadow: 10px 10px rgba(0,0,0,0.6);
+}
+</style>
