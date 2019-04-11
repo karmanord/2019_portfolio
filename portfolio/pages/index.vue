3623be8d9792db01e703edabda6836ef100c72f8
Y.Sasaki <y-sasaki@intworks.co.jp>
2019-04-11 23:37:04 +0900

    [replace]プロジェクトやり直し

---
+++ portfolio/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="container">
+    <Word />
+  </div>
+</template>
+
+<script>
+import Word from '~/components/word.vue'
+import {mapGetters, mapMutations} from 'vuex'
+
+export default {
+  components: {
+    Word
+  },
+  computed: {
+    ...mapGetters({
+      entered: 'entered'
+    })
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+#container {
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  text-align:center;
+
+  min-height: 100vh;
+}
+</style>
